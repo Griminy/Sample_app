237e05cd64b6f3a9972e5c81289dd2b80f12a785
grim <bozya003@gmail.com>
2014-11-08 01:04:17 +0200

    User data tests

---
+++ test/models/user_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  
+  def setup
+    @user=User.new(name:"Boy", email:"boy@e.com",
+                   password:"12345", password_confirmation: "12345")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should present" do
+    @user.name = "  "
+    assert_not @user.valid?
+  end
+
+  test "email should present" do
+    @user.email = "  "
+    assert_not @user.valid?
+  end
+
+  test "name should not be long" do
+    @user.name = "a" *51
+    assert_not @user.valid?
+  end 
+
+  test "email should not be long" do
+    @user.email = "a" *256
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid adresses" do
+    valid_adresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    valid_adresses.each do |valid_adress|
+      @user.email = valid_adress
+      assert @user.valid?, "#{valid_adress.inspect} should be valid"
+    end
+  end
+
+  test "invalid emails should be rejected" do
+    invalid_adresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com]
+    invalid_adresses.each do |invalid_adress|
+      @user.email = invalid_adress
+      assert_not @user.valid?, "#{invalid_adress.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" *4
+    assert_not @user.valid?
+  end
+end
