f38fdba8728b7c90bb5af621fe3810d5dd7c393e
grim <bozya003@gmail.com>
2014-11-10 01:55:59 +0200

    This frickin Log In/Out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController
+  class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome,Dude :P"
       redirect_to @user
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,24 +2,23 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
   
-  test "invalid signup iformation" do
+  test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do 
       post users_path, user: { name: "", email: "user@invalid",
                                password: "foo", password_confirmation:"notFoo" }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do
     get signup_path
-    assert_difference 'User.count' do 
-      post users_path, user: { name: "Example", email: "user@valid.com",
-                               password: "sucessed", password_confirmation:"sucessed" }
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name:  "Michael Exempelovich", email: "his@email.com",
+                                            password:"password",
+                                            password_confirmation: "password" }
     end
-    assert_template 'users/new'
-    assert_not flash.FILL_IN
+    assert_template 'users/show'
+    assert is_logged_in?
   end
 end
